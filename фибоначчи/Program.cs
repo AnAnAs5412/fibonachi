db1a85e0c62b5070b524d7a21f3ca42390fcf74d
Ananasik <Ananasik@ANANAS>
2023-12-17 16:08:09 +0500

    Добавьте файлы проекта.

---
+++ фибоначчи/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace фибоначчи
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите член ряда: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                int b = Fibonachi(i);
+                Console.WriteLine($"{i} член факториала = {b}");
+            }
+            int Fibonachi(int n)
+            {
+                if (n == 0 || n == 1)
+                {
+                    return n;
+                }
+                else
+                {
+                    return Fibonachi(n - 1) + Fibonachi(n - 2);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
